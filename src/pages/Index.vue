13e56446b3297179d2bff37f41cf438e2fa1c01f
KaoruFurusawa <kaoru.furusawa@yourmystar.jp>
2021-03-16 08:46:10 +0900

    feature challenge LV1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,67 @@
 <template>
-  <img alt="Vue logo" src="/~/assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>
+    modal challenge
+  </h1>
+  <button @click="showModal">表示</button>
+  <div v-show="activateModal">
+    <div
+      class="overlay"
+      @click="closeModal"
+    />
+    <div class="modal">
+      <h2>モーダル</h2>
+      <p>
+        テストテストテスト
+      </p>
+      <button @click="closeModal">閉じる</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '~/components/HelloWorld.vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  setup() {
+    const activateModal = ref(false)
+    
+    const showModal = () => {
+      activateModal.value = true
+    }
+
+    const closeModal = () => {
+      activateModal.value = false
+    }
+
+    return { activateModal, closeModal, showModal }
+  }
 })
 </script>
 
-<style></style>
+<style>
+.overlay {
+  background-color: #000;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 10;
+  transition: all .5s ease;
+  opacity: 0.7;
+}
+
+.modal {
+  padding: 15px;
+  width: 480px;
+  z-index: 15;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+}
+</style>
